caff3ff5ded94f7ef8920c43027a51adcd45db17
ngocthily <ngoctly94@gmail.com>
2021-02-08 13:13:56 -0800

    create express route handler

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
 
 // App Setup
 // registers them as middleware
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 3090;
