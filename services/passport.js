58f8eb43655ecaeb9c8960c091ce1183e771c6ff
ngocthily <ngoctly94@gmail.com>
2021-02-08 16:26:37 -0800

    add passport local

--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,26 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 
+// Create local strategy
+const localOptions = { usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+    // verify this username and password, call done with the user
+    // if it is the correct email and password
+    //  Otherwise call done
+    User.findOne({ email: email }, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+
+        // compare passwords
+        user.comparePassword(password, function(err, isMatch) {
+            if (err) { return done(err); }
+            if (!isMatch) { return done(null, false); }
+            return done(null, user);
+        })
+    })
+});
 // setup options for JWT Strategy
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
