5733e72f6f3bb56c97eaed8462bca82d83694cd8
ngocthily <ngoctly94@gmail.com>
2021-02-25 17:54:43 -0800

    add cors

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB setup
 mongoose.connect('mongodb://localhost:auth/auth', 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost:auth/auth',
 // App Setup
 // registers them as middleware
 app.use(morgan('combined'));
+app.use(cors());
 // app.use(bodyParser.json({ type: '*/*' }));
 app.use(bodyParser.urlencoded({
     extended: true
